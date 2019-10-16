2ccdb74587ef925a2651a0ea63356a3033af8925
pzombade <prashant_zombade@bmc.com>
2019-10-16 10:29:40 +0530

    New branch - Added custom bounds random method.

--- src/com/pzombade/Main.java
+++ src/com/pzombade/Main.java
@@ -7,5 +7,9 @@ public class Main {
     public static void main(String[] args) {
         int num = Utils.getRandomNumber();
 	    System.out.println("Hello Git from Intellij! Random number is "+num);
+
+	    int bound = 5;
+        num = Utils.getRandomNumber(bound);
+        System.out.println("Bound lower than "+bound + " is "+num);
     }
 }
